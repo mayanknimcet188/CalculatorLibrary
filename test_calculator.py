0d3505172709e716b99f844457f039b50b9381cd
Mayank Sharma <205119053@nitt.edu>
2021-04-20 01:33:20 +0530

    Added test for division

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 2 == calculator.divide(10, 5)
